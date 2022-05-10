a2e2636f1453df771c5f4205c069358f572c0a97
Cho Young Jun <whdudwns7100@naver.com>
2022-05-10 19:05:38 +0900

    프로필에서 비밀번호 변경

--- pybo/views/profile_views.py
+++ pybo/views/profile_views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, url_for, render_template, flash, g, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 from pybo.views.auth_views import login_required
 
 from pybo import db
-from pybo.forms import EditProfileForm
+from pybo.forms import EditProfileForm, EditPasswordForm
 from pybo.models import User
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
@@ -29,7 +30,7 @@ def edit():
             user.about_me = request.form['about_me']
             db.session.add(user)
             db.session.commit()
-            alt('프로필을 수정하였습니다.')
+            flash('프로필을 수정하였습니다.')
             return redirect(url_for('profile.profile'))
         except:
             print("profile edit error")
@@ -38,19 +39,21 @@ def edit():
 
 @bp.route('/reset/', methods=('GET', 'POST'))
 def reset():
+    print("/reset")
     form = EditPasswordForm()
     if request.method == 'POST' and form.validate_on_submit():
-        error = None
-        user = User.query.filter_by(email=request.args['id']).first()
+        user = User.query.filter_by(email=g.user.email).first()
         if not check_password_hash(user.password, form.password.data):
             error = '현재 비밀번호가 일치하지 않습니다'
-        elif not User:
-            user = generate_password_hash(form.password1.data)
-            error = '새 비밀번호가 일치하지 않습니다'
-        if error is None:
-            db.session.add(user)
-            db.session.commit()
-        flash("비밀번호가 변경되었습니다.")
-        return redirect(url_for('profile.profile'))
-    flash(error)
+            flash(error)
+        else :
+            if form.password1.data == form.password2.data:
+                user.password = generate_password_hash(form.password1.data)
+                flash("비밀번호가 변경되었습니다.")
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('profile.profile'))
+            else :
+                error = '새 비밀번호 재입력 해주세요.'
+                flash(error)
     return render_template('profile/profile_reset.html', form=form)
