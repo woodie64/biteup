9c45810e8f55fb8fc06b1f06756af59074df7847
woodie64 <onyu0604@gmail.com>
2022-05-11 00:13:27 +0900

    admin 페이지 추가 /admin

--- pybo/views/admin_views.py
+++ pybo/views/admin_views.py
@@ -7,3 +7,7 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 def admin():
     return render_template('admin/admin_form.html')
 
+
+@bp.route('/list/')
+def _list():
+    return render_template('admin/admin_list.html')
