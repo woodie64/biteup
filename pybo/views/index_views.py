c93ded2f478e46ea32f9841f7b8c8e95f4c808f0
woodie64 <onyu0604@gmail.com>
2022-05-22 13:07:01 +0900

    수정

--- pybo/views/index_views.py
+++ pybo/views/index_views.py
@@ -9,14 +9,31 @@ bp = Blueprint('index', __name__, url_prefix='/')
 
 
 @bp.route('/')
-def trade():
-    return render_template('index/trade.html')
-
-
-@bp.route('/')
 def _list():
+    # 입력 파라미터
+    page = request.args.get('page', type=int, default=1)
+    kw = request.args.get('kw', type=str, default='')
+
+    # 조회
     community_list = Community.query.order_by(Community.create_date.desc())
-    return render_template('index/trade.html', community_list=community_list)
+    if kw:
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(Answer.community_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+        community_list = community_list \
+            .join(User) \
+            .outerjoin(sub_query, sub_query.c.community_id == Community.id) \
+            .filter(Community.subject.ilike(search) |  # 제목
+                    Community.content.ilike(search) |  # 내용
+                    User.username.ilike(search) |  # 작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()
+
+    # 페이징
+    community_list = community_list.paginate(page, per_page=5)
+    return render_template('index/trade.html', community_list=community_list, page=page, kw=kw)
 
 
 @bp.route('/exchange')
